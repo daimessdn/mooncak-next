221b7bdb0e38df352681a6038c348aa8394af9df
daimessdn@gmail.com <daimessdn@gmail.com>
2024-07-18 18:00:49 +0700

    updated web title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1 className="text-2xl font-bold mb-4">Mooncak</h1>
+    <main className="mx-auto w-3/4">
+      <h1 className="text-2xl font-bold mb-4">Muncak</h1>
 
       <p>Your best travel agent to Puncak</p>
     </main>
